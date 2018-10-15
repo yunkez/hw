b32bad8f58b57e6437d4f8ee0233194780217e54
AlickLiu1 <springscenery.liu@hotmail.com>
2018-10-15 18:44:50 +0800

    using class weights for imbalanced training data

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ batch_size = 200
 num_classes = 2
 validation_ratio = 0.2
 t = 1
-
+num_samples = 20
 params = {'grid_size': grid_size,
           'batch_size': batch_size,
           'n_classes': num_classes,
@@ -20,13 +20,18 @@ params = {'grid_size': grid_size,
           'shuffle': True}
 
 # Datasets
-pro_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./training_data/*_pro_cg.pdb"))]
-lig_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./training_data/*_lig_cg.pdb"))]
-IDs = [(pro_labels[i], lig_labels[j]) for i in range(len(pro_labels)) for j in range(len(lig_labels))]
+# pro_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./training_data/*_pro_cg.pdb"))]
+# lig_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./training_data/*_lig_cg.pdb"))]
+# IDs = [(pro_labels[i], lig_labels[j]) for i in range(len(pro_labels)) for j in range(len(lig_labels))]
+
+pro_labels = [format(i + 1, '04') for i in range(100)]
+lig_labels = [format(i + 1, '04') for i in range(100)]
+IDs = [(pro_labels[i], lig_label) for i in range(len(pro_labels))
+       for lig_label in ([lig_labels[i]]+random.sample(list(set(lig_labels)-set(pro_labels[i])), num_samples-1))]
 
 class_weight = {
     0: 1.0,
-    1: 1.0 * len(pro_labels)*(len(lig_labels)-1)/len(pro_labels)
+    1: 1.0 * (num_samples-1)
 }
 
 print("starting to sample for validation partitions...")
@@ -49,7 +54,7 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 print("starting to fit model...")
 history = model.fit_generator(generator=training_generator, validation_data=validation_generator,steps_per_epoch=train_steps,validation_steps=val_steps, use_multiprocessing=True,workers=10,class_weight=class_weight,verbose=1, epochs=1)
 
-model.save('AtomNet.h5')
+model.save('AtomNet_100x20.h5')
 
 
 #print("starting to plot...")
