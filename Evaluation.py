12aeeab8a94751f1530add4b23511b841db0356f
yunkez <yunkezhou@gmail.com>
2018-10-28 19:43:30 +0800

    evaliation script

--- Evaluation.py
+++ Evaluation.py
@@ -9,13 +9,13 @@ grid_size = 24
 grid_resolution = 0.5
 grid_dim = math.floor(grid_size / grid_resolution)
 
-model = load_model('AtomNet_2000x10x5x1.0.h5', custom_objects={'auc': auc})
+model = load_model('AtomNet_3000x10x10x1.0x24x0.5.h5', custom_objects={'auc': auc})
 import numpy as np
 
 def getKey(item):
     return item[1]
 
-dic = load_obj('pro_lig_pairs_test')
+dic = load_obj('pro_lig_pairs_23')
 
 pro_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./test_data/*_pro_cg.pdb"))]
 lig_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./test_data/*_lig_cg.pdb"))]
