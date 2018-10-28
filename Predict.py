3209a8df8c10d03d317cf17f4e5e3e3b6645cddf
yunkez <yunkezhou@gmail.com>
2018-10-28 19:33:50 +0800

    prediction script

--- Predict.py
+++ Predict.py
@@ -10,8 +10,8 @@ def getKey(item):
 
 dic = load_obj('pro_lig_pairs_test')
 
-pro_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./testing_data_true/*_pro_cg.pdb"))]
-lig_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./testing_data_true/*_lig_cg.pdb"))]
+pro_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./testing_data_release/*_pro_cg.pdb"))]
+lig_labels = [os.path.basename(f)[:4] for f in sorted(glob.glob("./testing_data_release/*_lig_cg.pdb"))]
 IDs = [(pro_labels[i], lig_labels[j]) for i in range(len(pro_labels)) for j in range(len(lig_labels))]
 
 score_dic = {}
@@ -26,12 +26,24 @@ for pro in pro_labels:
 
     for lig in possible_lig_labels:
         X = np.empty((1, 48, 48, 48,4))
-        X[0], y_r = pro_lig_reader_sample(pro_label=pro, lig_label=lig, folder_name='testing_data_true', test=True)
+        X[0], y_r = pro_lig_reader_sample(pro_label=pro, lig_label=lig, folder_name='testing_data_release', test=True)
         y_p = model.predict(X)
         output['predicted_score'].append((lig, y_p[0][1]))
     output['predicted_score'] = sorted(output['predicted_score'], key=getKey, reverse=True)[:10]
-    dic[pro]=output['predicted_score']
+    score_dic[pro]=output['predicted_score']
     print(output)
-save_obj(dic, 'pro_lig_score')
+save_obj(score_dic, 'pro_lig_score')
 
 print("done")
+
+# dic = load_obj('pro_lig_score')
+
+with open('test_predictions.txt', 'w') as f:
+    header = '\t'.join(['pro_id','lig1_id','lig2_id','lig3_id','lig4_id','lig5_id','lig6_id','lig7_id','lig8_id','lig9_id','lig10_id'])
+    f.write(header + '\n')
+    for pro in [format(i + 1, '04') for i in range(len(pro_labels))]:
+        row = [int(pro)] + [int(i[0]) for i in score_dic[pro]]
+        s = '\t'.join([str(x) for x in row])
+        f.write(s + '\n')
+
+### pro_list = ['0030', '0035', '0038', '0052', '0057', '0063', '0067', '0090', '0105', '0111', '0117', '0118', '0124', '0157', '0162', '0184', '0194', '0246', '0267', '0295', '0306', '0307', '0310', '0314', '0344', '0358', '0406', '0412', '0438', '0444', '0449', '0467', '0487', '0504', '0520', '0564', '0570', '0574', '0599', '0601', '0605', '0616', '0638', '0639', '0654', '0666', '0684', '0706', '0722', '0725', '0753', '0754', '0812', '0815', '0821', '0824']
